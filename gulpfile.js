548c0a42c74699dc2dd81f67b2cc9e7c14695830
Seth <seth_wieder@condenast.com>
2015-12-17 14:53:47 -0500

    add scss support w/ livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require("gulp");
 var connect = require("gulp-connect");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var concat = require("gulp-concat");
 
 
 var PATH = {
   HTML_SRC : "./src/index.html",
-  HTML_OUT_DEV : "./dist/dev"
+  HTML_OUT_DEV : "./dist/dev",
+  SCSS_SRC : ["./src/scss/*.scss", "./src/components/**/*.scss"]
 }
 
 // Create a server at the Dev build location
@@ -23,13 +27,27 @@ gulp.task("htmlDev", function(){
     .pipe(connect.reload());
 });
 
+// Compile Sass and update livereload
+gulp.task('scssDev', function(){
+  gulp.src(PATH.SCSS_SRC)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('styles.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest("./dist/dev/css"))
+    .pipe(connect.reload());
+});
+
 // Watch files, run task when modified.
 gulp.task("watchDev", function(){
   
   // When src HTML changes, update dev HTML
   gulp.watch([PATH.HTML_SRC], ["htmlDev"]);
 
+  // When scss changes, re-compile
+  gulp.watch(PATH.SCSS_SRC, ['scssDev']);
 
 });
 
 gulp.task('default', ['connectDev', "watchDev"]);
+
